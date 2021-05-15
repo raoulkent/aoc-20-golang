140cf866e5299ccc7b79b0594139a692b5ce1124
Raoul Kent <raoulkent@gmail.com>
2021-05-15 17:00:45 +0200

    Complete day 1

--- days/day01.go
+++ days/day01.go
@@ -2,13 +2,36 @@ package days
 
 import (
 	"fmt"
+	"sort"
 
 	inputs "../inputs"
 )
 
 func One() {
 	inputSlice := inputs.Day1
+	var target = int(2020)
+	sort.Ints(inputSlice[:])
+	var a, b = WalkInward(inputSlice, target)
+	var multiplied int = a*b
+	fmt.Println(multiplied)
+}
 
-	outputSlice := inputSlice
-	fmt.Println(outputSlice)
+func WalkInward(arr []int, t int) (a, b int) {
+	var lhs int = 0
+	var rhs int = len(arr) - 1
+
+	for rhs > lhs {
+		if arr[rhs] > t {
+			rhs--
+		} else if arr[lhs] + arr[rhs] < t {
+			lhs++
+		} else if arr[lhs] + arr[rhs] > t {
+			rhs--
+		} else if arr[lhs] + arr[rhs] == t {
+			return arr[lhs], arr[rhs]
+		} else {
+			panic("Oh no!")
+		}
+	}
+	return
 }
